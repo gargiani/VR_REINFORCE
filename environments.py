cffbb4eab883b0fcfd518e722eeb0e8b5a4513d7
gargiani <gmatilde@control.ee.ethz.ch>
2022-01-11 11:31:36 +0100

    environments: interface to OpenAI Gym

--- environments.py
+++ environments.py
@@ -2,7 +2,10 @@ from methods import GPOMDP, NumericalMethod
 from policies import GaussianPolicy
 import torch
 import gym
+import random
 import numpy as np
+
+torch.backends.cudnn.deterministic=True
 
 class Environment:
 
@@ -13,10 +16,17 @@ class Environment:
 
     def set_seed(self):
 
-        if self.set_seed is not None:
-            self.env.seed(self.seed)
+        if self.env_seed is not None:
+            self.env.seed(self.env_seed)   
+            self.env.action_space.seed(self.env_seed)
 
-    def simulate(self, N, T, policy=None):
+            torch.manual_seed(0)
+            torch.cuda.manual_seed(0)
+            torch.random.manual_seed(0)
+            np.random.seed(0)
+            random.seed(0)
+
+    def simulate(self, N, policy=None, verbose=False):
 
         states_n = []
         actions_n = []
@@ -26,7 +36,8 @@ class Environment:
 
         for episode in range(N):
             
-            print("episode {} of {}\n".format(episode+1, N))
+            if verbose:
+                print("episode {} of {}\n".format(episode+1, N))
 
             done = False
 
@@ -35,7 +46,6 @@ class Environment:
             rewards = []
 
             observation = self.env.reset()
-            
 
             while not done:
                 
@@ -44,12 +54,12 @@ class Environment:
 
                 states.append(observation.tolist()) 
                 
-                if policy==None:
+                if policy==None:                    
                     action = self.env.action_space.sample()
+                    action = np.asarray(action)
                 else:
                     policy.distribution(torch.tensor([observation], dtype=torch.float32))
                     action = policy.sample()[0].numpy()
-
                 observation, reward, done, info = self.env.step(action)
                 
                 tot_reward += reward
@@ -83,13 +93,12 @@ class Pendulum(Environment):
     def __init__(self, render=False, seed=None):
         super().__init__(render, seed)
 
-        self.env = gym.make('Pendulum-v0')
+        self.env = gym.make('Pendulum-v1')
         self.state_space = ("Continuous", 3)
         self.action_space = ("Continuous", 1)
 
-
 #MountainCar
-class MountainCar(Environment):
+class MountainCarContinous(Environment):
 
     def __init__(self, render=False, seed=None):
         super().__init__(render, seed)
@@ -97,6 +106,26 @@ class MountainCar(Environment):
         self.env = gym.make('MountainCarContinuous-v0')
         self.state_space = ("Continuous", 2)
         self.action_space = ("Continuous", 1)
+
+#MountainCar-Discrete
+class MountainCar(Environment):
+
+    def __init__(self, render=False, seed=None):
+        super().__init__(render, seed)
+
+        self.env = gym.make('MountainCar-v0')
+        self.state_space = ("Continuous", 2)
+        self.action_space = ("Discrete", 3, [0,1,2])
+
+#Acrobot-Discrete
+class Acrobot(Environment):
+
+    def __init__(self, render=False, seed=None):
+        super().__init__(render, seed)
+
+        self.env = gym.make('Acrobot-v1')
+        self.state_space = ("Continuous", 6)
+        self.action_space = ("Discrete", 3, [0,1,2])
 
 #BipedalWalker
 class BipedalWalker(Environment):
@@ -131,133 +160,27 @@ class LunarLander(Environment):
 
 if __name__ == "__main__":
 
-    from policies import Neural_SoftMax, neuralnet
-    from methods import REINFORCE, GPOMDP
-
-    #CARTPOLE SCENARIO
+    #cartpole
     cartpole = CartPole(render=True)
+    cartpole.simulate(6)
 
-    state_size = cartpole.state_space[1]
-    action_size = cartpole.action_space[1]
-    
-    actions = torch.tensor(cartpole.action_space[2], dtype=torch.float32)
-
-    net = neuralnet(state_size+action_size, action_size, [32])
-    softmax_policy = Neural_SoftMax(net, actions)
-    
-    trajectories = cartpole.simulate(10, 100, policy=softmax_policy)
-
-    discount_factor = 0.9
-    method = GPOMDP(0.001, softmax_policy.neural_net.parameters, discount_factor)
-
-    for ii in range(10): 
-
-        observations = torch.tensor(trajectories["states"][ii])
-        actions = torch.tensor(trajectories["actions"][ii])   
-        rewards = torch.tensor(trajectories["rewards"][ii])
-        
-        logprob = softmax_policy.log_prob(observations, actions, 10)
-        method.grad_estimator(logprob, rewards)
-        
-    method.step()
-    method.reset_grad()
-
-    for param in softmax_policy.neural_net.parameters():
-        print(param.grad)
-
-    #PENDULUM
+    #pendulum
     pendulum = Pendulum(render=True)
+    pendulum.flag_terminal_reward = True
+    pendulum.simulate(3)
 
-    state_size = pendulum.state_space[1]
-    action_size = pendulum.action_space[1]
-    
-    net = neuralnet(state_size, action_size, [32])
-    gaussian_policy = GaussianPolicy(net, torch.eye(action_size))
-    
-    trajectories = pendulum.simulate(10, 100, policy=gaussian_policy)
-
-    discount_factor = 0.9
-    method = REINFORCE(0.001, gaussian_policy.mean.parameters, discount_factor)
-
-    for ii in range(10):
-
-        observations = torch.tensor(trajectories["states"][ii])
-        actions = torch.tensor(trajectories["actions"][ii])
-        rewards = torch.tensor(trajectories["rewards"][ii])
-
-        #date le osservazioni creo la distribuzione sulle azioni
-        logprob = gaussian_policy.log_prob(observations, actions, 10)
-        method.grad_estimator(logprob, rewards)
-
-    method.step()
-    method.reset_grad()
-
-    for param in gaussian_policy.mean.parameters():
-        print(param.grad)
-
-    import pdb; pdb.set_trace()
-    #MOUNTAINCAR
+    #mountaincar
     mountaincar = MountainCar(render=True)
+    mountaincar.simulate(3)
 
-    state_size = mountaincar.state_space[1]
-    action_size = mountaincar.action_space[1]
-    
-    net = neuralnet(state_size, action_size, [32])
-    gaussian_policy = GaussianPolicy(net, torch.eye(action_size))
-    
-    trajectories = mountaincar.simulate(10, 100, policy=gaussian_policy)
+    #acrobot
+    acrobot = Acrobot(render=True)
+    acrobot.simulate(3)
 
-    #BIPEDALWALKER
+    #bipedalwalker
     bipedalwalker = BipedalWalker(render=True)
+    bipedalwalker.simulate(3)
 
-    state_size = bipedalwalker.state_space[1]
-    action_size = bipedalwalker.action_space[1]
-    
-    net = neuralnet(state_size, action_size, [32])
-    gaussian_policy = GaussianPolicy(net, torch.eye(action_size))
-    
-    trajectories = bipedalwalker.simulate(10, 100, policy=gaussian_policy)
-
-    discount_factor = 0.9
-    method = REINFORCE(0.001, gaussian_policy.mean.parameters, discount_factor)
-
-    for ii in range(10):
-
-        observations = torch.tensor(trajectories["states"][ii])
-        actions = torch.tensor(trajectories["actions"][ii])
-        rewards = torch.tensor(trajectories["rewards"][ii])
-
-        #date le osservazioni creo la distribuzione sulle azioni
-        logprob = gaussian_policy.log_prob(observations, actions, 10)
-        method.grad_estimator(logprob, rewards)
-
-    method.step()
-    method.reset_grad()
-
-    for param in gaussian_policy.mean.parameters():
-        print(param.grad)
-
-    #LUNARLANDERCONTINUOUS
-    lunarlandercontinuous = LunarLanderContinuous(render=True)
-
-    state_size = lunarlandercontinuous.state_space[1]
-    action_size = lunarlandercontinuous.action_space[1]
-    
-    net = neuralnet(state_size, action_size, [32])
-    gaussian_policy = GaussianPolicy(net, torch.eye(action_size))
-
-    trajectories = lunarlandercontinuous.simulate(10, 100, policy=gaussian_policy)
-    
-    #LUNARLANDER
+    #lunarlander
     lunarlander = LunarLander(render=True)
-
-    state_size = lunarlander.state_space[1]
-    action_size = lunarlander.action_space[1]
-    
-    actions = torch.tensor(lunarlander.action_space[2], dtype=torch.float32)
-
-    net = neuralnet(state_size+action_size, action_size, [32])
-    softmax_policy = Neural_SoftMax(net, actions)
-
-    trajectories = lunarlander.simulate(10, 100, policy=softmax_policy)
-   
+    lunarlander.simulate(3)
